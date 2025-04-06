91f62a8e3c408eaf499464772bdaea8ee7f9af67
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-06 16:34:11 +0200

    Adjust padding

--- src/components/layout/sidebar/AccountCard.jsx
+++ src/components/layout/sidebar/AccountCard.jsx
@@ -3,11 +3,11 @@ import { user } from "../../../constants/index.jsx";
 
 export default function AccountCard() {
   return (
-    <div className="flex items-center gap-3 mb-4 hover:bg-link-hover p-3 pe-8 rounded-full me-3">
+    <div className="flex items-center gap-3 mb-4 hover:bg-link-hover p-3 rounded-full me-3">
       {user.img ? <img src={user.img} alt={user.name} />
-      : <div className="w-10 h-10 bg-purple-500 flex justify-center items-center rounded-full text-2xl">
-        {user.name.charAt(0)}
-      </div>}
+        : <div className="w-10 h-10 bg-purple-500 flex justify-center items-center rounded-full text-2xl">
+          {user.name.charAt(0)}
+        </div>}
       <div className="me-1">
         <span className="flex items-center gap-1 font-bold">{user.name} {user.isPrivate && <LockKeyhole size={17} />}</span>
         <span className="text-light text-sm">{user.username}</span>
--- src/components/layout/sidebar/index.jsx
+++ src/components/layout/sidebar/index.jsx
@@ -4,8 +4,8 @@ import AccountCard from './AccountCard.jsx';
 
 export default function Sidebar() {
   return (
-    <aside className="w-[275px] border-r-1 border-[#ffffff38] h-screen pt-[2px] ps-2 flex flex-col justify-between">
-      <div>
+    <aside className="w-[275px] border-r-1 border-[#ffffff38] h-screen pt-[2px] ps-2 flex flex-col justify-between overflow-y-scroll">
+      <div className='mb-4'>
         <NavLinks />
         <Button />
       </div>
