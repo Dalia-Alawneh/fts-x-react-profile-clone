7af592ea440aba809c6320f706e6d602a39018f0
Dalia Alawneh <alawnehdalia340@gmail.com>
2025-04-07 22:09:50 +0200

    Make tabs interactive

--- src/components/layout/tabbar/TabBar.jsx
+++ src/components/layout/tabbar/TabBar.jsx
@@ -1,19 +1,60 @@
-export default function TabBar() {
+export default function TabBar({ setActiveTab, activeTab }) {
+  const handleChangeTab = (tab) => {
+    setActiveTab(tab);
+  };
+
+  const getTabClasses = (tab) => {
+    return `inline-block p-4 ${
+      activeTab === tab
+        ? 'border-b-4 border-blue-primary text-main'
+        : 'text-light'
+    }`;
+  };
+
   return (
-      <div className="mb-4 border-b border-lighter flex w-full">
-        <ul className="flex w-full -mb-px text-sm font-medium text-center" id="default-styled-tab" role="tablist">
-          <li className="w-full hover:bg-link-hover" role="presentation">
-            <button className="inline-block p-4 border-b-5 border-blue-primary" id="posts-styled-tab" data-tabs-target="#styled-posts" type="button" role="tab"
-              aria-controls="posts"
-              aria-selected="false">Posts</button>
-          </li>
-          <li className="w-full hover:bg-link-hover" role="presentation">
-            <button className="inline-block p-4 text-light" id="replies-styled-tab" data-tabs-target="#styled-replies" type="button" role="tab" aria-controls="replies" aria-selected="false">Replies</button>
-          </li>
-          <li className="w-full ,hover:bg-link-hover" role="presentation">
-            <button className="inline-block p-4 text-light" id="media-styled-tab" data-tabs-target="#styled-media" type="button" role="tab" aria-controls="media" aria-selected="false">Media</button>
-          </li>
-        </ul>
-      </div>
-  )
+    <div className="mb-4 border-b border-lighter flex w-full">
+      <ul
+        className="flex w-full -mb-px text-sm font-medium text-center"
+        id="default-styled-tab"
+        role="tablist"
+      >
+        <li className="w-full hover:bg-link-hover" role="presentation">
+          <button
+            className={getTabClasses('posts')}
+            id="posts-styled-tab"
+            type="button"
+            role="tab"
+            aria-controls="posts"
+            onClick={() => handleChangeTab('posts')}
+          >
+            Posts
+          </button>
+        </li>
+        <li className="w-full hover:bg-link-hover" role="presentation">
+          <button
+            className={getTabClasses('replies')}
+            id="replies-styled-tab"
+            type="button"
+            role="tab"
+            aria-controls="replies"
+            onClick={() => handleChangeTab('replies')}
+          >
+            Replies
+          </button>
+        </li>
+        <li className="w-full hover:bg-link-hover" role="presentation">
+          <button
+            className={getTabClasses('media')}
+            id="media-styled-tab"
+            type="button"
+            role="tab"
+            aria-controls="media"
+            onClick={() => handleChangeTab('media')}
+          >
+            Media
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import WhoToFollow from "../components/features/followSuggestion/WhoToFollow.jsx";
 import Hashtags from "../components/features/hashtags/Hashtags.jsx";
 import MightLike from "../components/features/mightLike/MightLike.jsx";
@@ -12,6 +13,7 @@ import Search from "../components/ui/search/Search.jsx";
 import { page } from "../constants/index.jsx";
 
 export default function HomePage() {
+  const [activeTab, setActiveTab] = useState('posts')
   return (
     <div className="grid grid-cols-[1fr] sm:grid-cols-[80px_1fr] xl:grid-cols-[275px_1fr]">
       <header className="hidden sm:block w-full">
@@ -22,9 +24,13 @@ export default function HomePage() {
         <section className="grid grid-cols-[1fr] xl:grid-cols-[600px_378px] relative">
           <div className="border-e border-lighter">
             <PageHeader page={page} />
-            <TabBar />
-            <PostsList />
-            <WhoToFollow />
+            <TabBar setActiveTab={setActiveTab} activeTab={activeTab} />
+            {activeTab === "posts" && <>
+              <PostsList />
+              <WhoToFollow />
+            </>}
+            {activeTab === "replies" && <PostsList />}
+            {activeTab === "media" && <div>Media content</div>}
           </div>
           <section className="sticky top-0 hidden xl:flex w-full xl:w-[378px] h-screen overflow-y-auto right-0 pt-2 ps-6 flex-col gap-4">
             <Search />
@@ -34,7 +40,7 @@ export default function HomePage() {
             <Wrapper>
               <Hashtags />
             </Wrapper>
-            <Footer/>
+            <Footer />
           </section>
         </section>
       </main>
